9c17c01473ef4fa3802dd166138289fbee78433a
Paul <morningperson2@gmail.com>
2022-01-31 16:22:12 -0500

    Added i/o functionality to main to allow the entering of strings to a vector and print all strings from a vector.

--- Assignment_2/main.cpp
+++ Assignment_2/main.cpp
@@ -3,6 +3,36 @@
 
 int main()
 {
+  std::string temp = " ";
+  bool check = true;
+
+  std::vector<std::string> testStrings;
+  std::vector<std::string> resultString;
+
+  while (check)
+  {
+    std::cout << "Enter a string to add to a vector (Type STOP to end): ";
+    std::getline(std::cin, temp);
+    
+    if (temp == "STOP" || temp == "stop")
+    {
+      check = false;
+      continue;
+    }
+
+    testStrings.push_back(temp);
+  }
+
+ // resultString = longestLength(testStrings);
+
+  std::cout << "The longest string(s) is: ";
+
+  for (int i = 0; i < testStrings.size(); i++)
+  {
+    std::cout << testStrings[i] << ", ";
+  }
+
+  std::cout << std::endl;
 
   return 0;
 }
